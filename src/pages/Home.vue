60265abecba3ad75a0f541f2fb337a20c0b2cde0
weigo21 <feilaibu12@163.com>
2022-02-05 20:12:09 +0800

    嵌套路由

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,16 @@
 <template>
-    <h2>我是Home的内容</h2>
+    <div>
+        <h2>我是Home的内容</h2>
+        <ul class="nav nav-tabs">
+            <li>
+            <router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+            </li>
+            <li>
+            <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
